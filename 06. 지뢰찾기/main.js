3e96ab930935c3fc094e7ce990c5e047b64d04cd
gipark <gipark1993@gmail.com>
2020-03-12 03:43:10 +0900

    지뢰찾기

--- 06. 지뢰찾기/main.js
+++ 06. 지뢰찾기/main.js
@@ -1,4 +1,5 @@
 var dataset = [];
+var flag = false;
 document.querySelector("#exec").addEventListener("click", function() {
     var hor = document.querySelector("#hor").value;
     var ver = document.querySelector("#ver").value;
@@ -8,13 +9,15 @@ document.querySelector("#exec").addEventListener("click", function() {
     // 지뢰찾기 테이블 생성
     var tbody = document.querySelector("#table tbody");
     tbody.innerHTML='';
+    dataset=[];
+    openCnt = 0;
     for(var i=0; i<ver; i++) {
         var arr = [];
         dataset.push(arr);
         
         var tr = document.createElement("tr");
         for(var j=0; j<hor; j++) {
-            arr.push(1);
+            arr.push(0);
             
             var td = document.createElement("td");
 
@@ -44,16 +47,23 @@ document.querySelector("#exec").addEventListener("click", function() {
                 
             });
             td.addEventListener('click', function (e) {
+                if(flag) {
+                    return;
+                }
+                
                 var parentTr = e.currentTarget.parentNode;
                 var parentTable = parentTr.parentNode;
                 var tCell = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
                 var tRow = Array.prototype.indexOf.call(parentTable.children, parentTr);
-
+                
+                e.currentTarget.classList.add('opened');
                 if(dataset[tRow][tCell] === 'X') {
                     e.currentTarget.textContent = '펑';
+                    alert("실패");
+                    flag= true;
                 } else { // 주변 지뢰 개수
                     var tempArr = [
-                        dataset[tRow][tCell-1],                         dataset[tRow][tCell+1],
+                        dataset[tRow][tCell-1], dataset[tRow][tCell+1],
                     ];
                     if(dataset[tRow-1]) {
                         // concat : 배열과 배열을 합쳐 새로운 배열 반환
@@ -62,8 +72,44 @@ document.querySelector("#exec").addEventListener("click", function() {
                     if(dataset[tRow+1]){
                         tempArr = tempArr.concat(dataset[tRow+1][tCell-1],dataset[tRow+1][tCell],dataset[tRow+1][tCell+1]);
                     }
-                    console.log(tempArr);
-                    e.currentTarget.textContent = tempArr.filter(v => v==='X').length;
+                    // console.log(tempArr);
+                    var aroundMineLen = tempArr.filter(v => v==='X').length;
+                    e.currentTarget.textContent = aroundMineLen || '';  // aroundMineLen값이 거짓이면 ''
+                                                                        // 거짓인값 : '', 0, NaN, nulll, undefined, false
+                    dataset[tRow][tCell] = 1;
+
+                    // 주변 검사 (0인거 주변 칸 열기)
+                    if(aroundMineLen === 0) {
+                        var around = [];
+                        if(tbody.children[tRow-1]) {
+                            around = around.concat([
+                                tbody.children[tRow-1].children[tCell-1],
+                                tbody.children[tRow-1].children[tCell],
+                                tbody.children[tRow-1].children[tCell+1],
+                            ]);
+                        }
+                        around = around.concat([
+                            tbody.children[tRow].children[tCell-1],
+                            tbody.children[tRow].children[tCell+1],
+                        ]);
+                        if(tbody.children[tRow+1]) {
+                            around = around.concat([
+                                tbody.children[tRow+1].children[tCell-1],
+                                tbody.children[tRow+1].children[tCell],
+                                tbody.children[tRow+1].children[tCell+1],
+                            ])
+                        }
+                        around.filter(v => !!v) // undefined, null 체크
+                            .forEach(next => {
+                                var nextTr = next.parentNode;
+                                var nextTable = nextTr.parentNode;
+                                var nCell = Array.prototype.indexOf.call(nextTr.children, next);
+                                var nRow = Array.prototype.indexOf.call(nextTable.children, nextTr);
+                                if(dataset[nRow][nCell] !== 1){
+                                    next.click()
+                                }
+                            });
+                    }   
                 }
             })
             tr.appendChild(td);
